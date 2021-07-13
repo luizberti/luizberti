b44b6e57307204b9a4364cc509676db2a0a5cbea
Luiz Berti <luizberti@users.noreply.github.com>
2021-07-13 17:40:39 -0300

    Expand fish configuration

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,5 +3,17 @@ if status is-interactive
 
     alias vim=nvim
     alias gs='git status'
+    alias k=kubectl
+
+    alias ..='cd ..'
+    alias ...='cd ../..'
+    alias ....='cd ../../..'
+    alias .....='cd ../../../..'
+
+    alias ls='exa --group-directories-first'
+    alias la='exa --all --group-directories-first'
+    alias ll='exa --long --git --time-style=long-iso --group-directories-first'
+    alias lla='ll --all'
+    alias tre='exa --all --classify --tree --group-directories-first --ignore-glob=.git'
 end
 
